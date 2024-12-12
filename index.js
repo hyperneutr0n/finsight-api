b4c4efda144674d9ae1c8e2d1c5f26f00f3a7ba8
hyperneutr0n <randylemuel21@gmail.com>
2024-12-12 22:12:37 +0700

    updated cloudbuild

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 /**
- * Imports
+ * Import dependencies
  */
 require("dotenv").config();
+const fs = require('fs');
 const express = require("express");
+
+/**
+ * Setup profile-bucket-key
+ */
+if (process.env.NODE_ENV === 'production') {
+  const PROFILE_BUCKET_KEY_SECRET = process.env.PROFILE_BUCKET_KEY_VAL;
+  if (!PROFILE_BUCKET_KEY_SECRET) {
+    console.error('PROFILE_BUCKET_KEY is undefined');
+    process.exit(1);
+  }
+
+  fs.fileWrite('./profile-bucket-key.json', PROFILE_BUCKET_KEY_SECRET, (err) => {
+    if (err) {
+      console.error('Error writing profile-bucket-key.json');
+      process.exit(1);
+    }
+    console.log();
+  });
+}
+/**
+ * Import routing
+ */
 const defaultRoutes = require("./src/default/default.routes");
 const authRoutes = require("./src/auth/auth.routes");
 const userRoutes = require("./src/users/users.routes");
